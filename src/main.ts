90976dd6830b9574110253f533df5bef67ec88e6
iiivanpopov <iiivanpopov999@gmail.com>
2025-07-08 19:04:35 +0300

    chore: init

---
+++ src/main.ts
@@ -0,0 +1,51 @@
+import { NestFactory } from '@nestjs/core'
+import { AppModule } from './app.module'
+import * as fs from 'fs'
+import * as path from 'path'
+import { ValidationPipe } from '@nestjs/common'
+import { Response } from 'express'
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger'
+
+async function bootstrap() {
+  const app = await NestFactory.create(AppModule, {
+    httpsOptions: {
+      cert: fs.readFileSync(path.resolve(__dirname, '../credentials/cert.crt')),
+      key: fs.readFileSync(path.resolve(__dirname, '../credentials/key.key'))
+    },
+    cors: {
+      origin: '*'
+    }
+  })
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true,
+      forbidNonWhitelisted: true,
+      transform: true,
+      transformOptions: {
+        enableImplicitConversion: true
+      }
+    })
+  )
+
+  app.use('/live', (_req, res: Response) => {
+    res.json({ status: true })
+  })
+
+  const apiConfig = new DocumentBuilder()
+    .setVersion('1.0')
+    .addBearerAuth({
+      type: 'http',
+      scheme: 'bearer',
+      bearerFormat: 'JWT'
+    })
+    .build()
+
+  const document = SwaggerModule.createDocument(app, apiConfig)
+  SwaggerModule.setup('api', app, document)
+
+  const port = process.env.PORT ?? 3000
+  await app.listen(port)
+
+  console.log(`Application is running on: ${await app.getUrl()}`)
+}
+void bootstrap()
